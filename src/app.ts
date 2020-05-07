6166a7c0c1e713bc1b58ddc2c304dd07d295eb32
Fred <frederique.bordeleau@gmail.com>
2020-05-07 13:50:45 -0400

    Moduling code

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,3 @@
-// Drag and Drop Interfaces
-interface Draggable {
-  dragStartHandler(event: DragEvent): void;
-  dragEndHandler(event: DragEvent): void;
-}
-
-interface DragTarget {
-  dragOverHandler(event: DragEvent): void;
-  dropHandler(event: DragEvent): void;
-  dragLeaveHandler(event: DragEvent): void;
-}
-
 // Project Type
 enum ProjectStatus {
   Active,
